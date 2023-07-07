f1323b1463274c85b695588562201f71185c8c50
dgeric <dgeric@atrify.com>
2023-07-07 19:45:25 +0200

    Remove unnecessary method

--- util.ts
+++ util.ts
@@ -3,7 +3,3 @@ import {DateTime} from "luxon";
 export function formatBlock(startBlock: DateTime, endBlock: DateTime): string {
     return `${startBlock.toISO()} - ${endBlock.toISO()}`;
 }
-
-export function formatString(startBlock: String, endBlock: String): string {
-    return `${startBlock} - ${endBlock}`;
-}
